75017c3e95ad78b520f4175ce0678aa96b08930d
sarrkar <sarraah.karimi@gmail.com>
2023-08-30 06:58:57 -0700

    new test added

--- compare_cifar_10.sh
+++ compare_cifar_10.sh
@@ -1,11 +1,17 @@
-python train.py --seed 2 --epsilon 8 --alpha 8 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+# python train.py --seed 2 --epsilon 8 --alpha 8 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
 
-python train.py --seed 2 --vectorized --vectorized_low 0 --epsilon 8 --alpha 16 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
-python train.py --seed 2 --vectorized --vectorized_low 0.33 --epsilon 8 --alpha 12 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
-python train.py --seed 2 --vectorized --vectorized_low 0.6 --epsilon 8 --alpha 10 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+# python train.py --seed 2 --vectorized --vectorized_low 0 --epsilon 8 --alpha 16 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+# python train.py --seed 2 --vectorized --vectorized_low 0.33 --epsilon 8 --alpha 12 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+# python train.py --seed 2 --vectorized --vectorized_low 0.6 --epsilon 8 --alpha 10 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
 
 python train.py --seed 2 --epsilon 16 --alpha 8 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
 
 python train.py --seed 2 --vectorized --vectorized_low 0 --epsilon 16 --alpha 16 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
 python train.py --seed 2 --vectorized --vectorized_low 0.33 --epsilon 16 --alpha 12 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
 python train.py --seed 2 --vectorized --vectorized_low 0.6 --epsilon 16 --alpha 10 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+
+python train.py --seed 2 --epsilon 4 --alpha 8 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+
+python train.py --seed 2 --vectorized --vectorized_low 0 --epsilon 4 --alpha 16 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+python train.py --seed 2 --vectorized --vectorized_low 0.33 --epsilon 4 --alpha 12 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
+python train.py --seed 2 --vectorized --vectorized_low 0.6 --epsilon 4 --alpha 10 --unif 1 --dataset CIFAR10 --architecture preactresnet18 --out-dir results --root-model-dir models/
